f99a151c66ef58e3c6d9a5b826df894c36190182
Lynn-Mei <hugo.murura@gmail.com>
2023-06-21 13:26:36 +0200

    Mise a jour du projet initial vers monogame

---
+++ Hakumei/HMI/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new HMI.Game1();
+game.Run();
---
+++ Hakumei/HMI/SpriteBatchManager.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMI
+{
+    public class SpriteBatchManager
+    {
+        private static SpriteBatch spriteBatch;
+
+        public static SpriteBatch SpriteBatch { get => spriteBatch; }
+    }
+}
