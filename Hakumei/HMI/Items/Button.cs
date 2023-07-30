df64a2027a5a1524abc9c6ce9f169da0b55165ad
Lynn-Mei <hugo.murura@gmail.com>
2023-07-30 12:17:42 +0200

    Setup for Monogame
    Deleting former unity files and adding new monogame code

---
+++ Hakumei/HMI/Items/Button.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMI.Items
+{
+    /// <summary>
+    /// A standard graphical button
+    /// <author>Lynn Mei</author>
+    /// </summary>
+    public class Button:IDrawable
+    {
+        public Button()
+        {
+
+        }
+    }
+}
---
+++ Hakumei/HMI/Items/IDrawable.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMI.Items
+{
+    /// <summary>
+    /// An interface that generalizes the drawing behaviour of a graphic item
+    /// <author>Lynn Mei</author>
+    /// </summary>
+    public abstract class IDrawable
+    {
+        private static int x;
+        private static int y;
+
+        /// <summary>
+        /// Standard Method to draw a graphical item
+        /// </summary>
+        /// <author>Lynn Mei</author>
+        /// <returns>Success of the operation</returns>
+        public bool draw()
+        {
+            bool drawn = false;
+            try
+            {
+                //draw
+                drawn = true;
+            }catch(Exception e)
+            {
+                drawn = false;
+            }
+            return drawn;
+        }
+
+    }
+}
