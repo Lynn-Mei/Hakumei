5224e59d6a3548074e5c85da9ce64e1e7d3553ee
Lynn-Mei <hugo.murura@gmail.com>
2023-07-30 16:32:30 +0200

    Coordinates and Dimension classes

---
+++ Hakumei/Logic/Coordinates.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    /// <summary>
+    /// Generalizes items which have a set of coordinates as attributes
+    /// </summary>
+    public class Coordinates
+    {
+        protected int x;
+        protected int y;
+
+        /// <summary>
+        /// The Horizontal coordinate
+        /// </summary>
+        public int X { get { return x; } set { x = value; } }
+        /// <summary>
+        /// The Vertical coordinate
+        /// </summary>
+        public int Y { get { return y; } set { y = value; } }
+
+        public Coordinates(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        
+        }
+    }
+}
---
+++ Hakumei/Logic/Dimension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    /// <summary>
+    /// Generalizes objects with height and weight property
+    /// </summary>
+    public class Dimension:Coordinates
+    {
+        private int height;
+        private int width;
+
+        public int Height { get { return height; } set { height = value; } }
+        public int Width { get { return width; } set { width = value; } }
+
+        public Dimension(int x, int y, int height, int width):base(x,y) 
+        {
+            this.height = height;
+            this.width = width;
+        }
+    }
+}
